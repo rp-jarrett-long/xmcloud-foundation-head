ce19fc1ee5ad785158fdb6c3318c8b7439029d2d
Jarrett Long <jlong@rightpoint.com>
2024-03-12 16:57:43 -0500

    add grid order styles to page container

--- src/sxastarter/src/components/PageContainer.tsx
+++ src/sxastarter/src/components/PageContainer.tsx
@@ -2,21 +2,28 @@ import { ComponentRendering, Placeholder } from '@sitecore-jss/sitecore-jss-next
 import classNames from 'classnames';
 import { DynamicPlaceholderParams } from 'src/types/rendering-parameters/dynamic-placeholder-params';
 import { StandardParams } from 'src/types/rendering-parameters/standard-params';
-import { popStyles } from 'src/utils/pop-style';
+import { commonStyles, popStyles } from 'src/utils/pop-style';
 
 export const Default = (props: {
   rendering: ComponentRendering;
   params: StandardParams & DynamicPlaceholderParams;
 }): JSX.Element => {
-  const [styles, centerGrid, rowGutters] = popStyles(
+  const [styles, centerGrid, rowGutters, reverseGrid] = popStyles(
     props.params.styles,
     'column-grid--center',
-    'column-grid--row-gap'
+    'column-grid--row-gap',
+    RegExp(/column-grid--reverse-([^\s]+)/g),
+    commonStyles.colSpans // col-* classes are added by default by Sitecore, but not needed in this component.
   );
   return (
     <div className={`page-container ${styles}`} id={props.params.RenderingIdentifier ?? undefined}>
       <div
-        className={classNames('column-grid', centerGrid && centerGrid, rowGutters && rowGutters)}
+        className={classNames(
+          'column-grid',
+          centerGrid && centerGrid,
+          rowGutters && rowGutters,
+          reverseGrid && reverseGrid
+        )}
       >
         <Placeholder
           name={`page-container-${props.params.DynamicPlaceholderId}`}
